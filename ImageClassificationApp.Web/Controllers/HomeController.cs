8ba1f17afcabfaa4805740be0d72723f321cd66f
Aleksandar Petrov <mr.alexpetrov66@gmail.com>
2024-06-09 12:31:38 +0300

    Fix the project organization.

--- ImageClassificationApp.Web/Controllers/HomeController.cs
+++ ImageClassificationApp.Web/Controllers/HomeController.cs
@@ -24,39 +24,6 @@ namespace ImageClassificationApp.Web.Controllers
 			return View();
 		}
 
-		[HttpPost]
-		public async Task<IActionResult> ClassifyImage(IFormFile file)
-		{
-			if (file == null || file.Length == 0)
-				return Content("file not selected");
-
-			var fileName = $"{Guid.NewGuid()}_{file.FileName}";
-			var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", fileName);
-
-			Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads"));
-
-			using (var stream = new FileStream(filePath, FileMode.Create))
-			{
-				await file.CopyToAsync(stream);
-			}
-
-			byte[] imageBytes;
-			using (var memoryStream = new MemoryStream())
-			{
-				await file.CopyToAsync(memoryStream);
-				imageBytes = memoryStream.ToArray();
-			}
-
-			var sampleData = new MLModel_Sport.ModelInput { ImageSource = imageBytes };
-			var result = MLModel_Sport.Predict(sampleData);
-
-			float accuracy = result.Score.Max();
-
-			ViewData["ImagePath"] = $"/uploads/{fileName}";
-			ViewData["Accuracy"] = accuracy;
-			return View("SportImagesResult", result);
-		}
-
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ImageClassificationApp.Web/Controllers/SportImagesController.cs
@@ -0,0 +1,47 @@
+﻿using ImageClassificationApp_Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageClassificationApp.Web.Controllers
+{
+    public class SportImagesController : Controller
+    {
+        [HttpGet]
+        public IActionResult SportImages()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ClassifyImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", fileName);
+
+            Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads"));
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            byte[] imageBytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                imageBytes = memoryStream.ToArray();
+            }
+
+            var sampleData = new MLModel_Sport.ModelInput { ImageSource = imageBytes };
+            var result = MLModel_Sport.Predict(sampleData);
+
+            float accuracy = result.Score.Max();
+
+            ViewData["ImagePath"] = $"/uploads/{fileName}";
+            ViewData["Accuracy"] = accuracy;
+            return View("SportImagesResult", result);
+        }
+    }
+}
