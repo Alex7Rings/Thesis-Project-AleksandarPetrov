4befab4c1e51934d685da0aca6ff081dda5ce6ca
Aleksandar Petrov <mr.alexpetrov66@gmail.com>
2024-06-09 12:09:29 +0300

    Added Sport Image rclassification

--- ImageClassificationApp.Web/Controllers/HomeController.cs
+++ ImageClassificationApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ImageClassificationApp.Web.Models;
+using ImageClassificationApp_Web;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -17,8 +18,47 @@ namespace ImageClassificationApp.Web.Controllers
             return View();
         }
 
+		[HttpGet]
+		public IActionResult SportImages()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		[HttpPost]
+		public async Task<IActionResult> ClassifyImage(IFormFile file)
+		{
+			if (file == null || file.Length == 0)
+				return Content("file not selected");
+
+			var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+			var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", fileName);
+
+			Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads"));
+
+			using (var stream = new FileStream(filePath, FileMode.Create))
+			{
+				await file.CopyToAsync(stream);
+			}
+
+			byte[] imageBytes;
+			using (var memoryStream = new MemoryStream())
+			{
+				await file.CopyToAsync(memoryStream);
+				imageBytes = memoryStream.ToArray();
+			}
+
+			var sampleData = new MLModel_Sport.ModelInput { ImageSource = imageBytes };
+			var result = MLModel_Sport.Predict(sampleData);
+
+			float accuracy = result.Score.Max();
+
+			ViewData["ImagePath"] = $"/uploads/{fileName}";
+			ViewData["Accuracy"] = accuracy;
+			return View("SportImagesResult", result);
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ ImageClassificationApp.Web/Models/MLModel_Sport.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.IO;
+
+namespace YourNamespace.Models
+{
+    public class MLModel_Sport
+    {
+        public class ModelInput
+        {
+            [ColumnName("ImageSource"), LoadColumn(0)]
+            public byte[] ImageSource { get; set; }
+        }
+
+        public class ModelOutput
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabel { get; set; }
+
+            [ColumnName("Score")]
+            public float[] Score { get; set; }
+        }
+
+        private static Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(CreatePredictionEngine);
+
+        public static ModelOutput Predict(ModelInput input)
+        {
+            var predictionEngine = PredictionEngine.Value;
+            return predictionEngine.Predict(input);
+        }
+
+        private static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
+        {
+            var mlContext = new MLContext();
+            // Ensure the model path is correct and points to the new model file
+            string modelPath = @"C:\path\to\your\new\model.zip";
+            ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+            return predEngine;
+        }
+    }
+}
