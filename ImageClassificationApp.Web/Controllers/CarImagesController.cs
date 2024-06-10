6ed8f8dfb9ecf709e09b4e83f34fdeda05d7253f
Aleksandar Petrov <mr.alexpetrov66@gmail.com>
2024-06-10 16:22:39 +0300

    Implemented Car Image classification

---
+++ ImageClassificationApp.Web/Controllers/CarImagesController.cs
@@ -0,0 +1,47 @@
+﻿using ImageClassificationApp_Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageClassificationApp.Web.Controllers
+{
+    public class CarImagesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ClassifyImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", fileName);
+
+            Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads"));
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            byte[] imageBytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                imageBytes = memoryStream.ToArray();
+            }
+
+            var sampleData = new MLModel_Cars.ModelInput { ImageSource = imageBytes };
+            var result = MLModel_Cars.Predict(sampleData);
+
+            float accuracy = result.Score.Max();
+
+            ViewData["ImagePath"] = $"/uploads/{fileName}";
+            ViewData["Accuracy"] = accuracy;
+            return View("Result", result);
+        }
+    }
+}
